45ef2c20694f956f661e98bb9dcf97952d30b755
GitHubWison <15995448871@163.com>
2019-01-10 16:06:33 +0800

    数据库测试

--- lib/WanAndroidPage.dart
+++ lib/WanAndroidPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:wan_android_flutter/component/ArticlePadding.dart';
+import 'package:wan_android_flutter/component/DrawerPage.dart';
 import 'package:wan_android_flutter/pages/Discovery.dart';
 import 'package:wan_android_flutter/pages/Home.dart';
 
@@ -42,97 +43,7 @@ class WanAndroidPageState extends State<WanAndroidPage> {
   void _toSearch() {}
 }
 
-class DrawerPage extends StatelessWidget {
-  final drawerIcons = [
-    Icon(
-      Icons.favorite,
-      color: Colors.black,
-    ),
-    Icon(
-      Icons.settings,
-      color: Colors.black,
-    ),
-    Icon(
-      Icons.info_outline,
-      color: Colors.black,
-    ),
-    Icon(
-      Icons.power_settings_new,
-      color: Colors.black,
-    )
-  ];
-  static const textStyle = TextStyle(color: Colors.black, fontSize: 18);
-  final drawerTexts = [
-    Text(
-      '收藏',
-      style: textStyle,
-    ),
-    Text('设置', style: textStyle),
-    Text('关于我们', style: textStyle),
-    Text('退出', style: textStyle),
-  ];
 
-  @override
-  Widget build(BuildContext context) {
-    return ListView(
-      children: <Widget>[
-        _getUserInfo(),
-        _getItemWidget(drawerIcons[0], drawerTexts[0]),
-        _getItemWidget(drawerIcons[1], drawerTexts[1]),
-        _getItemWidget(drawerIcons[2], drawerTexts[2]),
-        _getItemWidget(drawerIcons[3], drawerTexts[3]),
-      ],
-    );
-  }
-
-  Widget _getItemWidget(Icon icon, Text text) {
-    return InkWell(
-      onTap: () {},
-      child: Padding(
-        padding: EdgeInsets.all(10),
-        child: Row(
-          children: <Widget>[
-            icon,
-            Padding(
-              padding: EdgeInsets.only(left: 10),
-              child: text,
-            )
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _getUserInfo() {
-    return Container(
-      color: Colors.blue,
-      child: Padding(
-        padding: EdgeInsets.all(20),
-        child: Column(
-          children: <Widget>[
-            CircleAvatar(
-              backgroundColor: Colors.black12,
-              minRadius: 0,
-              child: Padding(
-                padding: EdgeInsets.all(10),
-                child: Icon(
-                  Icons.person,
-                  color: Colors.white,
-                  size: 80,
-                ),
-              ),
-            ),
-            ArticlePadding(),
-            Text(
-              'username',
-              style: TextStyle(fontSize: 30, color: Colors.white),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
 
 class MainBodyState extends StatelessWidget {
 //  final currentPage;
---
+++ lib/component/DrawerPage.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:wan_android_flutter/component/ArticlePadding.dart';
+import 'package:wan_android_flutter/pages/FavoritePage.dart';
+class DrawerPage extends StatelessWidget {
+  final drawerIcons = [
+    Icon(
+      Icons.favorite,
+      color: Colors.black,
+    ),
+    Icon(
+      Icons.settings,
+      color: Colors.black,
+    ),
+    Icon(
+      Icons.info_outline,
+      color: Colors.black,
+    ),
+    Icon(
+      Icons.power_settings_new,
+      color: Colors.black,
+    )
+  ];
+  static const textStyle = TextStyle(color: Colors.black, fontSize: 18);
+  final drawerTexts = [
+    Text(
+      '收藏',
+      style: textStyle,
+    ),
+    Text('设置', style: textStyle),
+    Text('关于我们', style: textStyle),
+    Text('退出', style: textStyle),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: <Widget>[
+        _getUserInfo(),
+        _getItemWidget(drawerIcons[0], drawerTexts[0],onclick: (){
+          Navigator.of(context).push(MaterialPageRoute(builder: (context){
+            return FavoritePage();
+          }));
+        }),
+        _getItemWidget(drawerIcons[1], drawerTexts[1]),
+        _getItemWidget(drawerIcons[2], drawerTexts[2]),
+        _getItemWidget(drawerIcons[3], drawerTexts[3]),
+      ],
+    );
+  }
+
+  Widget _getItemWidget(Icon icon, Text text,{onclick}) {
+    return InkWell(
+      onTap: onclick,
+      child: Padding(
+        padding: EdgeInsets.all(10),
+        child: Row(
+          children: <Widget>[
+            icon,
+            Padding(
+              padding: EdgeInsets.only(left: 10),
+              child: text,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _getUserInfo() {
+    return Container(
+      color: Colors.blue,
+      child: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: <Widget>[
+            CircleAvatar(
+              backgroundColor: Colors.black12,
+              minRadius: 0,
+              child: Padding(
+                padding: EdgeInsets.all(10),
+                child: Icon(
+                  Icons.person,
+                  color: Colors.white,
+                  size: 80,
+                ),
+              ),
+            ),
+            ArticlePadding(),
+            Text(
+              'username',
+              style: TextStyle(fontSize: 30, color: Colors.white),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
